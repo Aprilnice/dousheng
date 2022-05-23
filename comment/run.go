055c1850de668a8b09d37f6fe454e9e2f7482db6
Aprilnice <hjw2001412@163.com>
2022-05-23 16:45:29 +0800

    update config

--- comment/run.go
+++ comment/run.go
@@ -3,6 +3,7 @@ package comment
 import (
 	"dousheng/comment/core"
 	"dousheng/comment/service"
+	"dousheng/config"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/registry/etcd"
@@ -11,15 +12,18 @@ import (
 )
 
 func Run() {
+	// 获取配置文件
+	commentConf := *(config.ConfInstance())
+	(&commentConf).WithServerConfig("comment")
 	// 注册件
 	etcdReg := etcd.NewRegistry(
-		registry.Addrs("192.168.141.101:2379"),
+		registry.Addrs(commentConf.EtcdConfig.Address),
 	)
 	// 得到一个微服务实例
 	microService := micro.NewService(
-		micro.Name("srv.comment"),
+		micro.Name(commentConf.ServerConfig.Name),
 		micro.Registry(etcdReg),
-		micro.Address("127.0.0.1:8088"),
+		micro.Address(commentConf.ServerConfig.Address),
 		micro.RegisterTTL(time.Second*10),
 		micro.RegisterInterval(time.Second*10),
 	)
--- config/config.go
+++ config/config.go
@@ -15,10 +15,11 @@ type (
 	// Config 配置文件结构体
 	Config struct {
 		vp *viper.Viper
-		*VideoServerConfig
+		*ServerConfig
 		*LogConfig
 		*MySQLConfig
 		*BaseConfig
+		*EtcdConfig
 	}
 
 	// BaseConfig 基础配置
@@ -60,12 +61,18 @@ type (
 		DefaultStringSize uint   `mapstructure:"default_string_size"`
 	}
 
-	// VideoServerConfig 视频配置服务
-	VideoServerConfig struct {
+	// EtcdConfig etcd配置
+	EtcdConfig struct {
+		// address 服务地址
+		Address string
+	}
+
+	// ServerConfig 配置服务
+	ServerConfig struct {
 		// Name 服务名
 		Name string
-		// Port 端口
-		Port string
+		// address 服务地址
+		Address string
 	}
 )
 
@@ -75,7 +82,7 @@ func NewConfig() *Config {
 	vp.SetConfigFile("config.yaml")
 	//viper.SetConfigName("config") // 1. 设置配置文件名字
 	//viper.SetConfigType("yaml")   // 2. 设置文件类型
-	vp.AddConfigPath(".") // 3. 配置文件路径
+	vp.AddConfigPath("./config") // 3. 配置文件路径
 	if err := vp.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			log.Fatalf("no such configs file: %v\n", err)
@@ -121,21 +128,32 @@ func (c *Config) WithBaseConfig() *Config {
 	return c
 }
 
-// WithVideoConfig 初始化视频服务配置
-func (c *Config) WithVideoConfig() *Config {
-	videoConf := VideoServerConfig{}
-	err := c.vp.UnmarshalKey("server.video", &videoConf)
+// WithEtcdConfig 初始化etcd配置
+func (c *Config) WithEtcdConfig() *Config {
+	etcdConf := EtcdConfig{}
+	err := c.vp.UnmarshalKey("etcd", &etcdConf)
+	if err != nil {
+		log.Fatalf("读取视频服务配置文件失败:%v\n", err)
+	}
+	c.EtcdConfig = &etcdConf
+	return c
+}
+
+// WithServerConfig 初始化服务配置
+func (c *Config) WithServerConfig(server string) *Config {
+	serverConf := ServerConfig{}
+	err := c.vp.UnmarshalKey("server."+server, &serverConf)
 	if err != nil {
 		log.Fatalf("读取视频服务配置文件失败:%v\n", err)
 	}
 	return &Config{
-		VideoServerConfig: &videoConf,
+		ServerConfig: &serverConf,
 	}
 }
 
 func Init() {
 	once.Do(func() {
-		conf = NewConfig().WithBaseConfig().WithLogConfig().WithMySQLConfig()
+		conf = NewConfig().WithBaseConfig().WithLogConfig().WithMySQLConfig().WithEtcdConfig()
 	})
 }
 
